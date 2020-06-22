0258b1340ef480bf62f3f43ce7b4908a8715748c
Petr Tobias <petr.tobias@plus4u.net>
2020-06-22 16:19:23 +0200

    Changed some variable names.

--- modern.php
+++ modern.php
@@ -3,14 +3,15 @@ class Log {
   function callback() {
     echo "Log messages:\n";
   }
-  function getLines(string $input, string $pattern, array $filters) : iterable {
-    $file = fopen($input, "r");
+  function getStats(string $inputFile, string $pattern, array $filters) : iterable {
+    $file = fopen($inputFile, "r");
     $stats = [];
+    $level = "";
     while($line = fgets($file)) {
       if (preg_match($pattern, $line, $matches)) {
         $level = strtolower($matches[1]);
       }
-      if (!in_array($level ?? '', $filters)) {
+      if (!in_array($level, $filters)) {
         if (array_key_exists($level, $stats)) {
           $stats[$level]++;
         }
@@ -23,15 +24,15 @@ class Log {
   }
 }
 $printLog = function ($stats) {
-  $lines = "";
+  $logLines = "";
   foreach ($stats as $level => $count) {
-    $lines .= "$level: $count\n";
+    $logLines .= "$level: $count\n";
   }
-  return $lines;
+  return $logLines;
 };
 
 // volání
 $log = new Log();
 call_user_func(array($log, 'callback'));
-$stats = $log->getLines('example.log', '/test\.(\w+)/', ['warning', 'alert']);
+$stats = $log->getStats('example.log', '/test\.(\w+)/', ['warning', 'alert']);
 echo $printLog($stats);
