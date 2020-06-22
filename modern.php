89c6d1dcfa50222c123d07126363f280df4d8918
Petr Tobias <petr.tobias@plus4u.net>
2020-06-22 16:14:34 +0200

    Improved modern php to include class, anonymous function and call of specified functions.

--- modern.php
+++ modern.php
@@ -1,30 +1,37 @@
 <?php
-function callback() {
-  echo "Log messages:\n";
-}
-function getLines(string $input, string $pattern, array $filters) {
-  $file = fopen($input, "r");
-  $stats = [];
-  while($line = fgets($file)) {
-    echo $line;
-    if (preg_match($pattern, $line, $matches)) {
-      $level = strtolower($matches[1]);
-    }
-    if (!in_array($level, $filters)) {
-      if (array_key_exists($level, $stats)) {
-        $stats[$level]++;
-      }
-      else {
-        $stats[$level] = 1;
-      }
-    }
+class Log {
+  function callback() {
+    echo "Log messages:\n";
   }
-  return $stats;
+  function getLines(string $input, string $pattern, array $filters) : iterable {
+    $file = fopen($input, "r");
+    $stats = [];
+    while($line = fgets($file)) {
+      if (preg_match($pattern, $line, $matches)) {
+        $level = strtolower($matches[1]);
+      }
+      if (!in_array($level ?? '', $filters)) {
+        if (array_key_exists($level, $stats)) {
+          $stats[$level]++;
+        }
+        else {
+          $stats[$level] = 1;
+        }
+      }
+    }
+    return $stats;
+  }
 }
+$printLog = function ($stats) {
+  $lines = "";
+  foreach ($stats as $level => $count) {
+    $lines .= "$level: $count\n";
+  }
+  return $lines;
+};
 
-$stats = getLines('example.log', '/test\.(\w+)/', ['warning', 'alert']);
-asort($stats);
-call_user_func('callback');
-foreach ($stats as $level => $count) {
-  echo "$level: $count\n";
-}
+// volání
+$log = new Log();
+call_user_func(array($log, 'callback'));
+$stats = $log->getLines('example.log', '/test\.(\w+)/', ['warning', 'alert']);
+echo $printLog($stats);
